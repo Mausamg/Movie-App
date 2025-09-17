a278cd4e3170f153e4f9ceee0722d575bfa84038
Mausam Shrestha <mausamshrestha1200@gmail.com>
2025-09-17 17:28:47 +0545

    add the route for the about page

--- src/routing/Index.jsx
+++ src/routing/Index.jsx
@@ -3,11 +3,13 @@ import Home from "../pages/Home";
 import Explorepage from "../pages/Explorepage";
 import Detailpage from "../pages/Detailpage";
 import Searchpage from "../pages/Searchpage";
+import About from "../pages/About";
 const Index = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
         <Route path=":explore" element={<Explorepage />} />
         <Route path=":explore/:id" element={<Detailpage />} />
         <Route path="search" element={<Searchpage />} />
